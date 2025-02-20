c79c9fc7980afaddf90386d4cfbef042a2e12e34
mivanov9348 <mivanov93@abv.bg>
2025-02-20 07:42:57 +0200

    Add draft function to add wrestler to brand

--- draft/urls.py
+++ draft/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import customize_draft, create_draft, drafts_list, delete_draft
+from .views import customize_draft, create_draft, drafts_list, delete_draft, add_wrestler_to_brand, auto_pick, \
+    finalize_draft
 
 app_name = 'draft'
 
 urlpatterns = [
     path('customize/', customize_draft, name='customize_draft'),
-    path('create/<int:draft_id>/', create_draft, name='create_draft'),
+    path('draft/<int:draft_id>/', create_draft, name='create_draft'),
+    path('add-wrestler/', add_wrestler_to_brand, name='add_wrestler'),
+    path('draft/auto-pick/<int:draft_id>/', auto_pick, name='auto_pick'),
+    path('draft/finalize/<int:draft_id>/', finalize_draft, name='finalize_draft'),
     path('list/', drafts_list, name='list_drafts'),
-path('delete/<int:draft_id>/', delete_draft, name='delete_draft'),
+    path('delete/<int:draft_id>/', delete_draft, name='delete_draft'),
 
 ]
