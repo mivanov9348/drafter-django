e58380502228944ecdda57f79ef4ec76481d470a
mivanov9348 <mivanov93@abv.bg>
2025-02-21 19:13:10 +0200

    add customize tournament and finsh the tournament

--- tournament/urls.py
+++ tournament/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 app_name = 'tournament'
 
+
 urlpatterns = [
     path('', views.tournaments_list, name='tournaments_list'),
     path('customize/', views.customize_tournament, name='customize_tournament'),
     path('tournament/<int:tournament_id>/', views.view_tournament, name='view_tournament'),
-    path('tournament/<int:tournament_id>/', views.view_tournament, name='view_tournament'),
     path('tournament/<int:tournament_id>/draw/', views.draw_bracket, name='draw_bracket'),
+    path('tournament/<int:tournament_id>/delete/', views.delete_tournament, name='delete_tournament'),
 ]
