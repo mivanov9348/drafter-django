a69a09101f61d8645365ea766e2be1533dda00db
mivanov9348 <mivanov93@abv.bg>
2025-02-21 11:53:00 +0200

    add random draw brackets tournament

--- tournament/urls.py
+++ tournament/urls.py
@@ -6,5 +6,7 @@ app_name = 'tournament'
 urlpatterns = [
     path('', views.tournaments_list, name='tournaments_list'),
     path('customize/', views.customize_tournament, name='customize_tournament'),
-path('tournament/<int:tournament_id>/', views.view_tournament, name='view_tournament'),
+    path('tournament/<int:tournament_id>/', views.view_tournament, name='view_tournament'),
+    path('tournament/<int:tournament_id>/', views.view_tournament, name='view_tournament'),
+    path('tournament/<int:tournament_id>/draw/', views.draw_bracket, name='draw_bracket'),
 ]
