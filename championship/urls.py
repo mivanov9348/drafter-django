d768aa42c74757fed1c427cd53ba4bcb03c2d716
mivanov9348 <mivanov93@abv.bg>
2025-02-24 08:25:24 +0200

    finish championship app and start manager_game

--- championship/urls.py
+++ championship/urls.py
@@ -6,4 +6,6 @@ app_name = 'championships'
 urlpatterns = [
     path('', views.championship_list, name='championship_list'),
     path('add/', views.add_championship, name='add_championship'),
+path('edit/<int:championship_id>/', views.edit_championship, name='edit_championship'),
+    path('delete/<int:championship_id>/', views.delete_championship, name='delete_championship'),
 ]
