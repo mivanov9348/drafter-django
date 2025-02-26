e7fcd5d52332883476c8e689cbec5b3ff531f898
mivanov9348 <mivanov93@abv.bg>
2025-02-26 07:30:26 +0200

    add game.html as calendar

--- manager_game/urls.py
+++ manager_game/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.game_redirect, name='game_redirect'),
     path('start/', views.start_game, name='start_game'),
     path('<int:game_id>/', views.game_dashboard, name='game_dashboard'),
+    path('<int:game_id>/next/', views.next_day, name='next_day'),
     path('<int:game_id>/ppv/<int:ppv_id>/simulate/', views.simulate_ppv, name='simulate_ppv'),
 ]
