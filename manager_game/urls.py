c7499e04cce2d51806c845a78c27dc4db42ff871
mivanov9348 <mivanov93@abv.bg>
2025-03-03 08:34:52 +0200

    add rivalries and functions

--- manager_game/urls.py
+++ manager_game/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     path('<int:game_id>/brands/', views.game_brands, name='game_brands'),
     path('<int:game_id>/power-rankings/', views.power_rankings, name='power_rankings'),
     path('<int:game_id>/championships/', views.championships, name='championships'),
-    path('<int:game_id>/rivalries/', views.rivalries, name='rivalries'),
     path('<int:game_id>/options/', views.options, name='options'),
     path('<int:game_id>/edit/', views.edit_game, name='edit_game'),
     path('<int:game_id>/reset/', views.reset_game, name='reset_game'),
+    path('<int:game_id>/rivalries/', views.game_rivalries, name='game_rivalries'),
 
 ]
