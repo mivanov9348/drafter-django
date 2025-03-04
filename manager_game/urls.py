6c744d84553fee122bc37b4c6074d514a92ad8a6
mivanov9348 <mivanov93@abv.bg>
2025-03-04 08:16:35 +0200

    extract content from game.html and make game_dashboard with days matches

--- manager_game/urls.py
+++ manager_game/urls.py
@@ -7,7 +7,8 @@ app_name = 'manager_game'
 urlpatterns = [
     path('', views.game_redirect, name='game_redirect'),
     path('start/', views.start_game, name='start_game'),
-    path('<int:game_id>/', views.game_dashboard, name='game_dashboard'),
+    path('<int:game_id>/', views.game, name='game'),  # Главна страница
+    path('<int:game_id>/dashboard/', views.game_dashboard, name='game_dashboard'),  # Dashboard
     path('<int:game_id>/next/', views.next_day, name='next_day'),
     path('<int:game_id>/ppv/<int:ppv_id>/simulate/', views.simulate_ppv, name='simulate_ppv'),
     path('<int:game_id>/remove/', remove_game, name='remove_game'),
@@ -19,5 +20,6 @@ urlpatterns = [
     path('<int:game_id>/edit/', views.edit_game, name='edit_game'),
     path('<int:game_id>/reset/', views.reset_game, name='reset_game'),
     path('<int:game_id>/rivalries/', views.game_rivalries, name='game_rivalries'),
-
+    path('<int:game_id>/rivalries/<int:rivalry_id>/delete/', views.delete_rivalry, name='delete_rivalry'),
+    path('<int:game_id>/dashboard/simulate_day/', views.simulate_day, name='simulate_day'),  # Simulate Day
 ]
